0ca43854891c2ca7fc7a255946229e050b0ffb88
Ioana Madalina Silai <madutza95@gmail.com>
2023-12-03 19:57:33 +0100

    Script itrameur pour le contexte roumain

--- programmes/make_itrameur_corpus.sh
+++ programmes/make_itrameur_corpus.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 
-LANG=$1
-FILE=$2
+FOLDER=$1
+LANG=$2
+URL_FILE="../urls/$LANG.txt"
 
 if [ $# -ne 2 ] # verifier si le script a un argument
 then
-	echo "ce programme demande deux arguments: le chemin vers le fichier ro.txt et un argument pour le chemin vers le fichier dumps créé" 
+	echo "ce programme demande deux arguments: un argument pour le chemin vers le dossier itrameur et la langue (version courte)" 
 	exit
 fi
 
-echo "<lang='ro'>" >$FILE #can I use single quotes here?
+echo "<lang='$LANG'>" >$FOLDER/dump-$LANG.txt
+echo "<lang='$LANG'>" >$FOLDER/contexte-$LANG.txt
 
 N=1
 
 while read -r line;
 do
-	echo "<page='ro-$N'>" >> $FILE
-	echo "<text>" >> $FILE
-	cat ../dumps-text/dump_ro_$N.txt | sed "s|&|&amp|g" | sed "s|<|&lt|g" | sed "s|>|&gt|g" >> $FILE
-	echo "</text>" >> $FILE
-	echo "</page>§" >> $FILE
+	echo "<page='$LANG-$N'>" >> $FOLDER/dump-$LANG.txt
+	echo "<page='$LANG-$N'>" >> $FOLDER/contexte-$LANG.txt
+	echo "<text>" >> $FOLDER/dump-$LANG.txt
+	echo "<text>" >> $FOLDER/contexte-$LANG.txt
+	cat ../dumps-text/dump_${LANG}_$N.txt | sed "s|&|&amp;|g" | sed "s|<|&lt;|g" | sed "s|>|&gt;|g" >> $FOLDER/dump-$LANG.txt
+	cat ../contexte/contexte_${LANG}_$N.txt | sed "s|&|&amp;|g" | sed "s|<|&lt;|g" | sed "s|>|&gt;|g" >> $FOLDER/contexte-$LANG.txt
+	echo "</text>" >> $FOLDER/dump-$LANG.txt
+	echo "</text>" >> $FOLDER/contexte-$LANG.txt
+	echo "</page>§" >> $FOLDER/dump-$LANG.txt
+	echo "</page>§" >> $FOLDER/contexte-$LANG.txt
 	N=$(expr $N + 1)
-done < $LANG
+done < $URL_FILE
 
-echo "</lang>" >> $FILE
+echo "</lang>" >> $FOLDER/dump-$LANG.txt
+echo "</lang>" >> $FOLDER/contexte-$LANG.txt
