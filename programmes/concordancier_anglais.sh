acb6d2ddce46650bb0edd42680487a388850ec47
Kehina Manseri <manserikehina@gmail.com>
2023-12-10 18:15:35 +0100

    Ajout test concordancier anglais

--- programmes/concordancier_anglais.sh
+++ programmes/concordancier_anglais.sh
@@ -4,7 +4,7 @@
 INPUT_PATH=$1
 OUTPUT_PATH=$2
 
-WORD_REGEX="(wars?)|(WARS?)"
+WORD_REGEX="(wars?)|(WARS?)|(Wars?)"
 
 echo "<!DOCTYPE html>
 <html>
@@ -23,7 +23,8 @@ echo "<!DOCTYPE html>
       </thead>
       <tbody>" > "$OUTPUT_PATH"
 
-CONCORDANCE_TABLE=$(cat $INPUT_PATH | sed '/^$/d'| egrep $WORD_REGEX | sed -E "s~(.*)($WORD_REGEX)(.*)~<tr><td>\1</td><td>\2</td><td>\9</td></tr>~g")
+CONCORDANCE_TABLE=$(cat $INPUT_PATH | sed '/^$/d'| egrep $WORD_REGEX | sed -E "s~(.*)($WORD_REGEX)(.*)~<tr><td>\1</td><td>\2</td><td>\6</td></tr>~g")
+#1 = .*, 2 = WORD_REGEX (ici trois groupes), 5 = suite.
 
 echo "$CONCORDANCE_TABLE" >> "$OUTPUT_PATH"
 
--- programmes/miniprojet_anglais.sh
+++ programmes/miniprojet_anglais.sh
@@ -16,13 +16,13 @@ while read -r line;
 do
 	ASPIRATION=$(curl -s -L ${line} > ../aspirations/url_en_$N.html)
 	DUMP=$(lynx -dump -nolist --display_charset=utf-8 ../aspirations/url_en_$N.html > ../dumps-text/dump_en_$N.txt)
-	WORDCOUNT=$(cat ../dumps-text/dump_en_$N.txt | egrep -o "(wars?)|(WARS?)"| wc -w)
-	cat ../dumps-text/dump_en_$N.txt | sed '/^$/d'| egrep -C 1 "(wars?)|(WARS?)" > ../contexte/contexte_en_$N.txt
+	WORDCOUNT=$(cat ../dumps-text/dump_en_$N.txt | egrep -o "(wars?)|(WARS?)|(Wars?)"| wc -w)
+	./concordancier_anglais.sh ../dumps-text/dump_en_$N.txt ../tableaux/concordances_anglais_$N.html
 	#obtenir le code HTTP and store it in a variable
 	CURL=$(curl -s -I -L ${line} | tr -d "\r" ) #obtenir les headers
 	HTTPCODE=$(echo "$CURL" | grep "^HTTP" | egrep -o "[[:digit:]]{3}" | tail -n 1)
 	ENCODING=$(echo "$CURL" | grep "^content-type:" | egrep -o "charset=[^;]*" | cut -f 2 -d =)
-	TABLEROW="<tr><td>$N</td><td>$HTTPCODE</td><td><a href=${line}>${line}</a></td><td>$ENCODING</td><td><a href="../aspirations/url_en_$N.html">aspiration</a></td><td><a href="../dumps-text/dump_en_$N.txt">dump</a></td><td>$WORDCOUNT</td><td><a href="../contexte/contexte_en_$N.txt">contexte</a></td></tr>"
+	TABLEROW="<tr><td>$N</td><td>$HTTPCODE</td><td><a href=${line}>${line}</a></td><td>$ENCODING</td><td><a href="../aspirations/url_en_$N.html">aspiration</a></td><td><a href="../dumps-text/dump_en_$N.txt">dump</a></td><td>$WORDCOUNT</td><td><a href="../contexte/contexte_en_$N.txt">contexte</a></td><td><a href="../tableaux/concordances_anglais_$N.html">concordances</a></td></tr>\n"
 	TABLE+="$TABLEROW\n"
 	N=$(expr $N + 1)
 done < "$FILE_PATH_IN"
