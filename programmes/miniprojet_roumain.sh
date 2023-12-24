cd57370b6193ad361e915a3c85800982288bb6a1
Ioana Madalina Silai <madutza95@gmail.com>
2023-12-24 22:01:38 +0000

    Change le script pour avoir les 3 tableaux pour les 3 types de guerre

--- programmes/miniprojet_roumain.sh
+++ programmes/miniprojet_roumain.sh
@@ -1,40 +1,61 @@
 #!/usr/bin/env bash
 
-FILE_PATH_IN=$1
-FILE_PATH_OUT=$2
+FILE_PATH_IN_UKRAINE=$1
+FILE_PATH_IN_ISRAEL=$2
+FILE_PATH_IN_GENERAL=$3
+FILE_PATH_OUT=$4
 
-if [ $# -ne 2 ] # verifier si le script a un argument
-then
-	echo "ce programme demande deux arguments: le chemin vers le fichier ro.txt et un argument pour le chemin vers le fichier html" 
-	exit
+if [ $# -ne 4 ]; then
+    echo "This script requires four arguments: paths to the files ro_ukraine.txt, ro_israel.txt, ro_general.txt, and the output HTML file."
+    exit 1
 fi
 
-N=1
-TABLE=""
-while read -r line;
-do
-	echo "Manipulation lien $N ..."
-	ASPIRATION=$(curl -s -L ${line} > ../aspirations/url_ro_$N.html)
-	DUMP=$(lynx -dump -nolist --display_charset=utf-8 ../aspirations/url_ro_$N.html > ../dumps-text/dump_ro_$N.txt)
-	WORDCOUNT=$(cat ../dumps-text/dump_ro_$N.txt | egrep -o "[Rr]ăzbo((iul)|(iului)|(aiele)|(aie)|(aielor)|i)"| wc -w)
-	cat ../dumps-text/dump_ro_$N.txt | sed '/^$/d'| egrep -C 1 "[Rr]ăzbo((iul)|(iului)|(aiele)|(aie)|(aielor)|i)" > ../contexte/contexte_ro_$N.txt
-	./concordancier_roumain.sh ../dumps-text/dump_ro_$N.txt ../tableaux/concordances_roumain_$N.html
-	#obtenir le code HTTP and store it in a variable 
-	CURL=$(curl -s -I -L ${line} | tr -d "\r" ) #obtenir les headers
-	HTTPCODE=$(echo "$CURL" | grep "^HTTP" | egrep -o "[[:digit:]]{3}" | tail -n 1)
-	ENCODING=$(echo "$CURL" | grep "^content-type:" | egrep -o "charset=[^;]*" | cut -f 2 -d =)
-	TABLEROW="<tr><td>$N</td>\n"
-	TABLEROW+="<td>$HTTPCODE</td>\n"
-	TABLEROW+="<td><a href=${line}>${line}</a></td>\n"
-	TABLEROW+="<td>$ENCODING</td>\n"
-	TABLEROW+="<td><a href="../aspirations/url_ro_$N.html">aspiration</a></td>\n"
-	TABLEROW+="<td><a href="../dumps-text/dump_ro_$N.txt">dump</a></td>\n"
-	TABLEROW+="<td>$WORDCOUNT</td>\n"
-	TABLEROW+="<td><a href="../contexte/contexte_ro_$N.txt">contexte</a></td>\n"
-	TABLEROW+="<td><a href="../tableaux/concordances_roumain_$N.html">concordances</a></td>\n"
-	TABLEROW+="</tr>"
-	TABLE+="$TABLEROW\n"
-	N=$(expr $N + 1)
-done < "$FILE_PATH_IN"
+process_file() {
+    local FILE_PATH=$1
+    local TABLE=""
+    local TYPE=$2
+    local N=1
 
-sed "s|<!-- Table -->|$TABLE|g" template_roumain.html > "$FILE_PATH_OUT"
+    while read -r line; 
+    do
+        echo "Processing link $N for $FILE_PATH..."
+        ASPIRATION=$(curl -s -L "${line}" > ../aspirations/url_ro_${TYPE}_$N.html)
+        DUMP=$(links -dump ../aspirations/url_ro_${TYPE}_$N.html > ../dumps-text/dump_ro_${TYPE}_$N.txt)
+        WORDCOUNT=$(cat ../dumps-text/dump_ro_${TYPE}_$N.txt | egrep -o "[Rr]ăzbo((iul)|(iului)|(aiele)|(aie)|(aielor)|i)"| wc -w)
+        cat ../dumps-text/dump_ro_${TYPE}_$N.txt | sed '/^$/d'| egrep -C 1 "[Rr]ăzbo((iul)|(iului)|(aiele)|(aie)|(aielor)|i)" > ../contexte/contexte_ro_${TYPE}_$N.txt
+        ./concordancier_roumain.sh ../dumps-text/dump_ro_${TYPE}_$N.txt ../tableaux/concordances_roumain_${TYPE}_$N.html
+        #obtenir le code HTTP and store it in a variable 
+        CURL=$(curl -s -I -L "${line}" | tr -d "\r" ) #obtenir les headers
+        HTTPCODE=$(echo "$CURL" | grep "^HTTP" | egrep -o "[[:digit:]]{3}" | tail -n 1)
+        ENCODING=$(echo "$CURL" | grep "^content-type:" | egrep -o "charset=[^;]*" | cut -f 2 -d =)
+
+        TABLEROW="<tr><td>$N</td>\n"
+        TABLEROW+="<td>$HTTPCODE</td>\n"
+        TABLEROW+="<td><a href=\"${line}\">${line}</a></td>\n"
+        TABLEROW+="<td>$ENCODING</td>\n"
+        TABLEROW+="<td><a href=\"../aspirations/url_ro_${TYPE}_$N.html\">aspiration</a></td>\n"
+        TABLEROW+="<td><a href=\"../dumps-text/dump_ro_${TYPE}_$N.txt\">dump</a></td>\n"
+        TABLEROW+="<td>$WORDCOUNT</td>\n"
+        TABLEROW+="<td><a href=\"../contexte/contexte_ro_${TYPE}_$N.txt\">contexte</a></td>\n"
+        TABLEROW+="<td><a href=\"../tableaux/concordances_roumain_${TYPE}_$N.html\">concordances</a></td>\n"
+        TABLEROW+="</tr>"
+        TABLE+="$TABLEROW\n"
+
+        N=$((N + 1))
+    done < "$FILE_PATH"
+
+    # Replace the placeholder in the HTML template using awk
+    awk -v table="$TABLE" '/<!-- Table '"$TYPE"' -->/{print table; next} 1' "$FILE_PATH_OUT" > "$FILE_PATH_OUT.tmp"
+    mv "$FILE_PATH_OUT.tmp" "$FILE_PATH_OUT"
+}
+
+
+generate_file() {
+    cat template_roumain.html > "$FILE_PATH_OUT"
+}
+
+# Process each file separately
+generate_file
+process_file "$FILE_PATH_IN_UKRAINE" "Ukraine"
+process_file "$FILE_PATH_IN_ISRAEL" "Israel"
+process_file "$FILE_PATH_IN_GENERAL" "General"
